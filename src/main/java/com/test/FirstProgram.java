52e875f4a7009da70ff603dfb2d41ae24cc0c0e7
santosh <skp10281@gmail.com>
2024-06-11 22:45:50 +0530

    First project

--- src/main/java/com/test/FirstProgram.java
+++ src/main/java/com/test/FirstProgram.java
@@ -3,5 +3,7 @@ package com.test;
 public class FirstProgram {
     public static void main(String args[]){
         System.out.println("*** Hello *** ");
+
+        System.out.println("Welcome to Java World");
     }
 }
