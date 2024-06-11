fe9163d629ed9c326d33165c33bfcd9a263aa94a
santosh <skp10281@gmail.com>
2024-06-11 22:50:36 +0530

    new class added

---
+++ src/main/java/com/test/SecondProgram.java
@@ -0,0 +1,9 @@
+package com.test;
+
+public class SecondProgram {
+    public static void main(String args[]){
+        System.out.println("*** Hello *** ");
+
+        System.out.println("Welcome to Java World");
+    }
+}
